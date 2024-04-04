a49283e6c387ca8340d7909c83709982c52b32ea
fatimah <fatimahbintiyasin5@gmail.com>
2024-04-04 12:52:14 -0400

    First commit

---
+++ Fatimah_COMP303_Assignment1/src/main/java/com/example/demo/FatimahComp303Assignment1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatimahComp303Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatimahComp303Assignment1Application.class, args);
+	}
+
+}
