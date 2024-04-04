8461c89c4e8a3734efecf8cd6ee683ba8d5d0c96
Benton Le <ble21@my.centennialcollege.ca>
2024-04-04 13:48:08 -0400

    Display page showing Tax and Discount

--- Fatimah_COMP303_Assignment1/src/main/java/com/example/demo/CruiseController.java
+++ Fatimah_COMP303_Assignment1/src/main/java/com/example/demo/CruiseController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CruiseController {
@@ -14,6 +15,8 @@ public class CruiseController {
 		double price = initialPrice * cruise.getStaterooms();
 		double discount = cruise.calculateDiscount();
 		double tax = cruise.calculateTax();
+		
+		System.out.println(cruise.getAgeGroup());
 		
 		double totalPrice = (price - discount) + tax;
 		
@@ -31,6 +34,8 @@ public class CruiseController {
 		m.addAttribute("guests", cruise.getGuests());
 		m.addAttribute("price", initialPrice);
 		m.addAttribute("totalPrice", totalPrice);
+		m.addAttribute("discount", discount);
+		m.addAttribute("tax", tax);
 		
 		return "show-cruise";
 	}
